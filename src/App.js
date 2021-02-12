d6b3b2a6f926334aac87a10be2aed300c9ba78e1
Svitlana <rsv0011@gmail.com>
2021-02-12 17:40:17 +0100

    init project
    create the main structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import Navbar from './components/layout/Navbar';
+import Index from './components/layout/Index';
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+
+      <div className="container">
+        <Switch>
+          <Route exact path='/' component={Index} />
+        </Switch>
+      </div>
+      
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/tracks/Tracks.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function Tracks(){
+  return(
+    <>
+    </>
+  )
+}export default Tracks;
